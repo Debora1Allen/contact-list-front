83f4076dd523753307f5f5ac6c25a610ad9ff925
Silva <deboraaaline21@gmail.com>
2024-09-11 20:49:54 -0300

    Fix: tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the form to add a person', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const formElement = screen.getByPlaceholderText(/Person Name/i);
+  expect(formElement).toBeInTheDocument();
 });
+
+test('renders the button to submit form', () => {
+  render(<App />);
+  const submitButton = screen.getByText(/Submit/i);
+  expect(submitButton).toBeInTheDocument();
+});
+
